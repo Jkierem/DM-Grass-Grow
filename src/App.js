8e4491109fe17055a15eb4b0cb59c880254994d3
Jkierem <juanmig8@hotmail.com>
2018-03-24 15:42:55 -0500

    Base react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component{
+  render(){
+    return(
+    <header>
+      <h1>
+        Hello World!
+      </h1>
+      <p>
+        I am now alive and ready to React.
+      </p>
+    </header>)
+  }
+}
+
+export default App;
